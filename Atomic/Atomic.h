6446caf75a12bccdfc9b1af81586a1340b968db4
Adlai Holler <guy.on.internet@icloud.com>
2015-12-06 12:32:45 -0800

    Remove silly import of UIKit

--- Atomic/Atomic.h
+++ Atomic/Atomic.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Adlai Holler. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Atomic.
 FOUNDATION_EXPORT double AtomicVersionNumber;
